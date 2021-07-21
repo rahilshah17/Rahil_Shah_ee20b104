ecc7efa2d5dbb41feaf25dcab4073d4ef05e31f1
rahilshah17 <rahiljuly02@gmail.com>
2021-07-21 10:05:47 +0530

    first gh page

---
+++ src/title.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function t1(){
+  return(
+      <div className = 'home-div'>
+        <a href="./a1.jsx">A1</a>
+        <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a href="./a2">A2</a>
+        <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <a href="/sandbox/src/a1.jsx">A3</a>
+        <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+      </div>
+
+  )
+}
+
+export default t1;
